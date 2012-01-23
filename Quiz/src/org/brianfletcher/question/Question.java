111a453050eb733ecdf8d6c8f65d40a2413f9081
punkle <brian.jf.fletcher@gmail.com>
2012-01-23 23:20:16 +0000

    prevent duplicate descriptions for a question

--- Quiz/src/org/brianfletcher/question/Question.java
+++ Quiz/src/org/brianfletcher/question/Question.java
@@ -8,13 +8,26 @@ public class Question {
 	
 	public Question(FactParser factParser ){
 		facts = new Fact[3];
-		ArrayList<Fact> allFacts = new ArrayList<Fact>(factParser.getFacts());
-		for(int i = 0; i < facts.length; i++ ){
-			int selectedFact = (int) (Math.random() * (allFacts.size()));
-			facts[i] = allFacts.remove(selectedFact);
-		}
 		
-		answerFactId =  (int) (Math.random() * 3);
+		do {
+			ArrayList<Fact> allFacts = new ArrayList<Fact>(factParser.getFacts());
+			for(int i = 0; i < facts.length; i++ ){
+				int selectedFact = (int) (Math.random() * (allFacts.size()));
+				facts[i] = allFacts.remove(selectedFact);
+			}
+			answerFactId =  (int) (Math.random() * 3);
+		} while ( testDuplicateFacts() );
+	}
+	
+	public boolean testDuplicateFacts(){
+		if (facts[0].getDescription().equals(facts[1].getDescription())){
+			return true;
+		} else if (facts[0].getDescription().equals(facts[2].getDescription())){
+			return true;
+		} else if (facts[1].getDescription().equals(facts[2].getDescription())){
+			return true;
+		}
+		return false;
 	}
 	
 	public Fact[] getFacts(){

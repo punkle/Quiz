6debde99de2d10d679a4be16128f7dba6ba5562b
Brian Fletcher <brian.jf.fletcher@gmail.com>
2012-01-28 00:48:12 +0000

    using PhoneGap for addview

--- Quiz/src/org/brianfletcher/AddWebView.java
+++ Quiz/src/org/brianfletcher/AddWebView.java
@@ -1,18 +1,18 @@
 package org.brianfletcher;
 
-import android.app.Activity;
+import com.phonegap.*;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.WindowManager;
 
-public class AddWebView extends Activity {
-	private WebView mWebView;
+public class AddWebView extends DroidGap {
 	 
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.addwebview);
-
-	    mWebView = (WebView) findViewById(R.id.webview);
-	    mWebView.getSettings().setJavaScriptEnabled(true);
-	    mWebView.loadUrl(getString(R.string.add_url));
+	    
+	    super.loadUrl(getString(R.string.add_url));
+	    
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN | 
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 	}
 }
